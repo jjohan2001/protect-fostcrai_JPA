e979dc70ea51683cf312c1db39cb66da6c7b870c
jjohan2001 <99340026+jjohan2001@users.noreply.github.com>
2022-11-03 19:01:52 -0500

    hay un error preguntele al profe por que enteoria deberia funcionar no hay nada raro

--- protect-fostcrai_JPA/appDemo/src/main/java/com/fostcraiv2/appDemo/controlador/ActividadControlador.java
+++ protect-fostcrai_JPA/appDemo/src/main/java/com/fostcraiv2/appDemo/controlador/ActividadControlador.java
@@ -1,5 +1,6 @@
 package com.fostcraiv2.appDemo.controlador;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,51 +17,5 @@ public class ActividadControlador {
 
 	@Autowired
 	private ActividadService service;
-	
-	@GetMapping({"actividades", "/"})
-	public String listarActvidades(Model modelo) {
-		modelo.addAttribute("actividaes", service.listarTodasLasActividades());
-		return "actividades";
-	}
-	
-	@GetMapping("/actividades/nueva")
-	public String mostrarFromularioActividades(Model model) {
-		Actividad actividad = new Actividad();
-		model.addAttribute("actividad", actividad);
-		return "crear_Actividad";
-	}
-	
-	@PostMapping("/actividades")
-	public String guardarActividad(@ModelAttribute("actividad") Actividad actividad) {
-		service.guardarActividad(actividad);
-		return "redirect:actividades";
-	}
-	
-	@GetMapping("/actividades/editar/{id}")
-	public String mostrarFormEditar(@PathVariable Long id, Model model) {
-		model.addAttribute("actividad", service.obtenerActividadporId(id));
-		return "editar_actividad";
-	}
-	
-	@PostMapping("/actividades/{id}")
-	public String actualizar(@PathVariable Long id, @ModelAttribute("acividad") Actividad actividad, Model model) {
-		Actividad actividadExistente = service.obtenerActividadporId(id);
-		actividadExistente.setId(id);
-		actividadExistente.setActividades(actividad.getActividades());
-		actividadExistente.setSolicitudes(actividad.getSolicitudes());
-		
-		
-		service.actualizarActividad(actividadExistente);
-		return "redirect:/actividades";
-	}
-	
-	@GetMapping("/actividades/{id}")
-	public String eliminarActividad(@PathVariable Long id) {
-		service.eliminarActividad(id);
-		return "redirect:/actividades";
-	}
-	
-	
-	
 	
 }
